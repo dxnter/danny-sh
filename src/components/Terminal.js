1ac33ce01a583ad399e8dca0d372d22e8d326e93
dxnter <contact@danfoster.io>
2020-01-30 17:59:30 -0600

    Formatting

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Draggable from 'react-draggable';
 
-const Terminal = () => (
-  <Draggable>
-    <div
-      id="termynal"
-      data-termynal
-      data-ty-typedelay="40"
-      data-ty-linedelay="40"
-    >
-      <span data-ty="input">npm install personal-info</span>
-      <span data-ty="progress" />
-      <span data-ty>Successfully installed personal-info</span>
-      <span data-ty="input">node</span>
-      <span data-ty="input" data-ty-prompt=">">
-        {"const personalInfo = require('personal-info')"}
-      </span>
-      <span data-ty="input" data-ty-prompt=">">
-        personalInfo.findByGithub('dxnter')
-      </span>
-      <span data-ty>{'{'}</span>
-      <span data-ty>{"    name: 'Danny Foster',"}</span>
-      <span data-ty>{'    age: 21,'}</span>
-      <span data-ty>
-        {'    github: '}
-        <a href="https://github.com/dxnter">'dxnter'</a>
-        {','}
-      </span>
-      <span data-ty>
-        {'    website: '}
-        <a href="https://danfoster.io">'danfoster.io'</a>
-      </span>
-      <span data-ty>{'}'}</span>
-    </div>
-  </Draggable>
-);
+class Terminal extends Component {
+  componentDidMount() {
+    console.log(document.querySelector('#termynal', '::before'));
+  }
+
+  render() {
+    return (
+      <Draggable>
+        <div
+          id="termynal"
+          data-termynal
+          data-ty-typedelay="40"
+          data-ty-linedelay="40"
+        >
+          <span data-ty="input">npm install personal-info</span>
+          <span data-ty="progress" />
+          <span data-ty>Successfully installed personal-info</span>
+          <span data-ty="input">node</span>
+          <span data-ty="input" data-ty-prompt=">">
+            {"const personalInfo = require('personal-info')"}
+          </span>
+          <span data-ty="input" data-ty-prompt=">">
+            personalInfo.findByGithub('dxnter')
+          </span>
+          <span data-ty>{'{'}</span>
+          <span data-ty>{"    name: 'Danny Foster',"}</span>
+          <span data-ty>{'    age: 21,'}</span>
+          <span data-ty>
+            {'    github: '}
+            <a href="https://github.com/dxnter">'dxnter'</a>
+            {','}
+          </span>
+          <span data-ty>
+            {'    website: '}
+            <a href="https://danfoster.io">'danfoster.io'</a>
+          </span>
+          <span data-ty>{'}'}</span>
+        </div>
+      </Draggable>
+    );
+  }
+}
 
 export default Terminal;
