e85a84f49287c6b172175a112a0dcb6f2f35cbdf
dxnter <contact@danfoster.io>
2020-02-24 22:44:50 -0600

    Mobile responsive

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Draggable from 'react-draggable';
 
 class Terminal extends Component {
   componentDidMount() {
@@ -8,62 +7,60 @@ class Terminal extends Component {
 
   render() {
     return (
-      <Draggable>
-        <div
-          id="termynal"
-          data-termynal
-          data-ty-typedelay="40"
-          data-ty-linedelay="40"
-        >
-          <span data-ty="input">npm install personal-info</span>
-          <span data-ty="progress" />
-          <span data-ty>Successfully installed personal-info</span>
-          <span data-ty="input">node</span>
-          <span data-ty="input" data-ty-prompt=">">
-            {"const personalInfo = require('personal-info')"}
-          </span>
-          <span data-ty="input" data-ty-prompt=">">
-            personalInfo.findByGithub('dxnter')
-          </span>
-          <span data-ty>{'{'}</span>
-          <span data-ty>{"    name: 'Danny Foster',"}</span>
-          <span data-ty>
-            {'    blog: '}
-            <a
-              href="https://danfoster.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              'danfoster.io'
-            </a>
-            {','}
-          </span>
-          <span data-ty>
-            {'    github: '}
-            <a
-              href="https://github.com/dxnter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              'dxnter'
-            </a>
-            {','}
-          </span>
-          <span data-ty>
-            {'    twitter: '}
-            <a
-              href="https://twitter.com/tower"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              'tower'
-            </a>
-            {','}
-          </span>
-          <span data-ty>{"    discord: 'dxnter#1600',"}</span>
-          <span data-ty>{'}'}</span>
-        </div>
-      </Draggable>
+      <div
+        id="termynal"
+        data-termynal
+        data-ty-typedelay="40"
+        data-ty-linedelay="40"
+      >
+        <span data-ty="input">npm install personal-info</span>
+        <span data-ty="progress" />
+        <span data-ty>Successfully installed personal-info</span>
+        <span data-ty="input">node</span>
+        <span data-ty="input" data-ty-prompt=">">
+          {"const personalInfo = require('personal-info')"}
+        </span>
+        <span data-ty="input" data-ty-prompt=">">
+          personalInfo.findByGithub('dxnter')
+        </span>
+        <span data-ty>{'{'}</span>
+        <span data-ty>{"    name: 'Danny Foster',"}</span>
+        <span data-ty>
+          {'    blog: '}
+          <a
+            href="https://danfoster.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            'danfoster.io'
+          </a>
+          {','}
+        </span>
+        <span data-ty>
+          {'    github: '}
+          <a
+            href="https://github.com/dxnter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            'dxnter'
+          </a>
+          {','}
+        </span>
+        <span data-ty>
+          {'    twitter: '}
+          <a
+            href="https://twitter.com/tower"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            'tower'
+          </a>
+          {','}
+        </span>
+        <span data-ty>{"    discord: 'dxnter#1600',"}</span>
+        <span data-ty>{'}'}</span>
+      </div>
     );
   }
 }
